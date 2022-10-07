583f915679f635f6c38c56e3c5040684bbf63167
Ashudeep Dubey <ashudeepdubey8108@gmail.com>
2022-10-07 22:37:36 +0530

    initial commit
    Created a react app and made some changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>
+          aisocietywebsite
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
